f85cf31b02b5bc40112c46ff1a9e6bd50c574b94
Abhicoder786 <117636060+abhiCoderShek@users.noreply.github.com>
2024-11-04 18:45:03 +0530

    first commmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
 
 
-
-import PrivateRoute from "./Components/PrivateRoute";
-import SignIn from "./pages/SignIn";
 import Home from "./pages/Home";
-import PublicRoute from "./Components/PublicRoute";
-import { ProfileProvider } from "./context/profile.context";
-import { Routes, Route } from "react-router-dom";
-import { useState, useEffect } from "react";
 
 
 
@@ -18,27 +11,7 @@ import { useState, useEffect } from "react";
 function App() {
   return (
     <>
-    <ProfileProvider>
-      <Routes>
-      <Route element = {<PublicRoute/>}>
-      <Route path = "/signin" element={<SignIn/>}></Route>
-
-      </Route>
-      
-      
-
-    
-      <Route element = {<PrivateRoute/>}>
-      <Route path = "/" element={<Home/>}></Route>
-
-      </Route>
-      
-        
-      </Routes>
-    
-   
-    </ProfileProvider>
-    
+    <Home/>
       
     </>
   );
